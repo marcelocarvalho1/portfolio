00486134382f761e183e4b0d6b57bf2661a1e7ee
Marcelo <marceloo.dev@gmail.com>
2025-05-09 21:44:35 -0300

    Refatorando estilos e classes de botão para melhor consistência visual e responsividade

--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -12,16 +12,7 @@ import BOOTSTRAP from "../../assets/tech/bootstrap.svg";
 import SASS from "../../assets/tech/sass.svg";
 import REACT from "../../assets/tech/react.svg";
 
-const habilidades = [
-  html5,
-  CSS,
-  JS,
-  GIT,
-  GITHUB,
-  BOOTSTRAP,
-  SASS,
-  REACT,
-];
+const habilidades = [html5, CSS, JS, GIT, GITHUB, BOOTSTRAP, SASS, REACT];
 
 const floatAnimation = {
   y: [0, -20, 0],
@@ -36,11 +27,9 @@ const Intro = () => {
   return (
     <section className="intro">
       <div className="container">
-        
-
         <img src={Img} alt="imagem" className="topo-image" />
         <h1>
-          Olá, eu sou o <span>Marcelo</span>
+          Olá, sou <span>Marcelo Carvalho</span>
         </h1>
         <p>Desenvolvedor</p>
         <div className="m-auto-btn">
@@ -52,21 +41,21 @@ const Intro = () => {
         </div>
       </div>
       <div className="habilidades-flutuantes">
-          {habilidades.map((icon, index) => (
-            <motion.img
-              key={index}
-              src={icon}
-              alt={`icon-${index}`}
-              className="icone-flutuante"
-              animate={floatAnimation}
-              style={{
-                top: `${Math.random() * 80}%`,
-                left: `${Math.random() * 90}%`,
-                animationDelay: `${index * 0.5}s`,
-              }}
-            />
-          ))}
-        </div>
+        {habilidades.map((icon, index) => (
+          <motion.img
+            key={index}
+            src={icon}
+            alt={`icon-${index}`}
+            className="icone-flutuante"
+            animate={floatAnimation}
+            style={{
+              top: `${Math.random() * 80}%`,
+              left: `${Math.random() * 90}%`,
+              animationDelay: `${index * 0.5}s`,
+            }}
+          />
+        ))}
+      </div>
     </section>
   );
 };
